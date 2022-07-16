d12122359595fdcf30695ee341b5087f9cfc12c1
ggrossvi <hello.gloria@gmail.com>
2022-07-15 19:11:49 -0700

    react_spread_operators

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,24 +10,11 @@ function App() {
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setContact(prevValue => {
+    setContact((prevValue) => {
       if (name === "fName") {
         return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value
+          ...prevValue,
+          [name]: value // need bracket notation to get val out
         };
       }
     });
